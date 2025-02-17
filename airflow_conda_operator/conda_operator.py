59a65db62e3c1484b3334bf374e4011b8abeb67d
Achim Gaedke <achim.gaedke@lynker-analytics.com>
2025-02-18 08:07:14 +1300

    update operator code

--- airflow_conda_operator/conda_operator.py
+++ airflow_conda_operator/conda_operator.py
@@ -1,7 +1,8 @@
 import os
 import tempfile
 from pathlib import Path
-from typing import Sequence
+import subprocess
+from typing import Sequence, Any
 
 from airflow.operators.python import ExternalPythonOperator
 from jinja2 import Environment, PackageLoader, select_autoescape
@@ -12,31 +13,35 @@ class CondaPythonOperator(ExternalPythonOperator):
     Run a function in a mamba/conda env.
     """
 
-    template_fields: Sequence[str] = tuple({"conda_env", "conda_root_prefix"} | set(ExternalPythonOperator.template_fields))
+    template_fields: Sequence[str] = tuple(
+        {"conda_env", "conda_root_prefix"}
+        | set(ExternalPythonOperator.template_fields) - {"python"}
+    )
 
     def __init__(
         self,
         *,
-        python="python",
         conda_env: str = "base",
         conda_root_prefix: str | None = None,
         **kwargs,
     ):
+        # todo: alternatively, get this from the output of `conda --json`` field "conda_prefix"
         if conda_root_prefix is None:
             conda_root_prefix = Path(os.environ["CONDA_EXE"]).parents[1]
         else:
             conda_root_prefix = Path(conda_root_prefix)
-        if not (conda_root_prefix / "bin" /"activate").is_file():
+        if not (conda_root_prefix / "bin" / "activate").is_file():
             raise ValueError(f"`activate` script not found in {conda_root_prefix}")
         self.conda_root_prefix = str(conda_root_prefix)
         self.conda_env = conda_env
-        self.interpreter_name = python
+        self.interpreter_name = "python"
+
         super().__init__(
-            python=python,
+            python=self._get_python_path(),
             **kwargs,
         )
 
-    def execute_callable(self):
+    def _create_python_stub(self) -> str:
         templating_env = Environment(
             loader=PackageLoader("airflow_conda_operator"),
             autoescape=select_autoescape(),  # todo: check this
@@ -52,15 +57,34 @@ class CondaPythonOperator(ExternalPythonOperator):
         python_stub = tempfile.NamedTemporaryFile(
             prefix=python_stub_prefix, suffix=".sh", delete=False
         )
+        python_stub.write(python_stub_script.encode())
+        python_stub.close()
         try:
-            python_stub.write(python_stub_script.encode())
-            python_stub.close()
-            # might go wrong, some OS do not allow executables in /tmp
             os.chmod(python_stub.name, 0o755)
-            self.python = python_stub.name
-            try:
-                return super().execute_callable()
-            finally:
-                self.python = self.interpreter_name
+        except PermissionError:
+            # might go wrong, some OS/distributions do not allow executables in /tmp
+            # Add error hint: consider setting TMPDIR environment variable
+            raise
+        return python_stub.name
+
+
+    def _get_python_path(self) -> str:
+        """
+        get the full executable path of the python interpreter
+        """
+        python_stub = self._create_python_stub()
+        try:
+            return subprocess.check_output(
+                [python_stub, "-c", "import sys; print(sys.executable)"],
+                executable=python_stub,
+                text=True,
+            ).strip()
         finally:
-            os.unlink(python_stub.name)
+            os.unlink(python_stub)
+
+    def _execute_python_callable_in_subprocess(self, _) -> (Any | None):
+        python_stub = self._create_python_stub()
+        try:
+            return super()._execute_python_callable_in_subprocess(python_stub)
+        finally:
+            os.unlink(python_stub)
