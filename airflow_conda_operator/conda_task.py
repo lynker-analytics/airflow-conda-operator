d364329ac044aa316f98176b6c9aaaa8decca560
Achim Gädke <135793393+AchimGaedkeLynker@users.noreply.github.com>
2026-05-31 17:46:33 +1200

    Update conda_task.py

--- airflow_conda_operator/conda_task.py
+++ airflow_conda_operator/conda_task.py
@@ -37,26 +37,3 @@ def conda_task(
         decorated_operator_class=_CondaPythonDecoratedOperator,
         **kwargs,
     )
-
-
-def get_provider_info():
-    return {
-        "package-name": "airflow-conda-operator",
-        "name": "Conda",
-        "description": "Conda/Mamba environment integration",
-        "integrations": [
-            {"integration-name": "Conda"},
-        ],
-        "task-decorators": [
-            {
-                "name": "conda",
-                "class-name": "airflow_conda_operator.conda_task.conda_task",
-            }
-        ],
-        "operators": [
-            {
-                "integration-name": "Conda",
-                "python-modules": ["airflow_conda_operator.conda_operator"],
-            }
-        ],
-    }
